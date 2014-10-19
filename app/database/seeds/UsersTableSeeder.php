1170b94fb258cc4573832015f7fc09b5d035d527
William Blommaert <williamblommaert@gmail.com>
2014-10-19 15:39:25 +0200

    Created seed for users table

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder {
+
+	public function run()
+	{
+        Eloquent::unguard();
+
+        $faker = Faker::create();
+        $adminPass = Hash::make('cookiejar');
+
+        $user = User::create(array(
+            'email' => 'william.blommaert@student.kdg.be',
+            'photo' => $faker->imageUrl($width = 100, $height = 100, 'cats'),
+            'username' => 'William',
+            'password' => $adminPass
+        ));
+
+        $user = User::create(array(
+            'email' => 'ksenia.karelskaya@student.kdg.be',
+            'photo' => $faker->imageUrl($width = 100, $height = 100, 'cats'),
+            'username' => 'Ksenia',
+            'password' => $adminPass
+        ));
+
+		foreach(range(1, 48) as $index)
+		{
+			User::create([
+                'email' => $faker->email,
+                'photo' => $faker->imageUrl($width = 100, $height = 100, 'cats'),
+                'username' => $faker->userName,
+                'password' => Hash::make($faker->word)
+			]);
+		}
+	}
+
+}
