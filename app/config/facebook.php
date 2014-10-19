617a3da37ccf46a38b1fb7fa68af5cb58a59de21
William Blommaert <williamblommaert@gmail.com>
2014-10-19 23:13:47 +0200

    Added working Facebook login and routing

---
+++ app/config/facebook.php
@@ -0,0 +1,7 @@
+<?php
+
+// Facebook app Config
+return array(
+    'appId' => '697947863631184',
+    'secret' => '2d51cea840cf9f90f998e8cb5301b05a'
+);
--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,9 @@
 <?php
-Route::get('login', 'SessionController@create');
-Route::get('logout', 'SessionController@destroy');
-Route::resource('sessions', 'SessionController');
 
-// Home routes
-Route::get('/', function() { return View::make('hello'); });
+// Logging in/out using Cookie Jar account
+Route::get('login', 'SessionController@create');
+//Route::get('logout', 'SessionController@destroy');
+Route::resource('sessions', 'SessionController');
 
 // Search routes
 Route::get('search', 'SearchController@home');
@@ -27,3 +26,79 @@ Route::get('messages', 'MessageController@home');
 Route::get('questions', 'QuestionController@home');
 Route::get('questions/details', 'QuestionController@details');
 Route::get('questions/add-new', 'QuestionController@create');
+
+// Facebook routes
+Route::get('/', function()
+{
+    $data = array();
+
+    if (Auth::check()) {
+        $data = Auth::user();
+    }
+    return View::make('user', array('data'=>$data));
+});
+
+Route::get('logout', function() {
+    Auth::logout();
+    return Redirect::to('/');
+});
+
+Route::get('login/fb', function() {
+    $facebook = new Facebook(Config::get('facebook'));
+    $params = array(
+        'redirect_uri' => url('/login/fb/callback'),
+        'scope' => 'email',
+    );
+    return Redirect::away($facebook->getLoginUrl($params));
+});
+
+Route::get('login/fb/callback', function() {
+    $code = Input::get('code');
+    if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
+
+    $facebook = new Facebook(Config::get('facebook'));
+    $uid = $facebook->getUser();
+
+    if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
+
+    $me = $facebook->api('/me');
+
+    dd($me);
+});
+
+Route::get('login/fb/callback', function() {
+    $code = Input::get('code');
+    if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
+
+    $facebook = new Facebook(Config::get('facebook'));
+    $uid = $facebook->getUser();
+
+    if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
+
+    $me = $facebook->api('/me');
+
+    $profile = Profile::whereUid($uid)->first();
+    if (empty($profile)) {
+
+        $user = new User;
+        $user->username = $me['first_name'].' '.$me['last_name'];
+        $user->email = $me['email'];
+        $user->photo = 'https://graph.facebook.com/'.$me['id'].'/picture?type=large';
+
+        $user->save();
+
+        $profile = new Profile();
+        $profile->uid = $uid;
+        $profile->username = $me['id'];
+        $profile = $user->profiles()->save($profile);
+    }
+
+    $profile->access_token = $facebook->getAccessToken();
+    $profile->save();
+
+    $user = $profile->user;
+
+    Auth::login($user);
+
+    return Redirect::to('/')->with('message', 'Logged in with Facebook');
+});
