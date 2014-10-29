45b0aeda35d2b7328cac09106937a000601d211c
Ksenia Karelskaya <kusksu@gmail.com>
2014-10-29 20:45:20 +0100

    Fully styled registration. Note: check FB registration button

--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.default')
 
 @section('content')
-        {{ Form::open(['route' => 'user.store', 'method' => 'post', 'files' => true]) }}
+<br/>
+<div class="md-overlay"></div> 
+<div class="md-modal md-effect md-show" id="modal">
+    <div class="md-content">
+        {{ Form::open(['route' => 'user.store', 'method' => 'post', 'files' => true, 'class' => 'form-group']) }}
+        <h1>Sign Up</h1>
+            <div class='btn fb big'>
+                <a href="/login/fb">Sign Up  with Facebook</a>
+            </div> 
+            <div class="separator">
+                <span class="div-line" ></span><p class="divider xs">or</p><span class="div-line" ></span>
+            </div>   
             <div>
-                {{ Form::text('firstname', null, ['placeholder' => 'First Name', 'class' => 'form-field', 'required' => 'required']) }}
-                {{ Form::text('lastname', null, ['placeholder' => 'Last Name', 'class' => 'form-field', 'required' => 'required']) }}
+                {{ Form::text('firstname', null, ['placeholder' => 'First Name', 'class' => 'form-field form-field-xs left', 'required' => 'required']) }}
+                {{ Form::text('lastname', null, ['placeholder' => 'Last Name', 'class' => 'form-field form-field-xs', 'required' => 'required']) }}
             </div>
             <div>
                 {{ Form::text('email', null, ['placeholder' => 'Email address', 'class' => 'form-field', 'required' => 'required']) }}
@@ -13,15 +24,15 @@
                 {{ Form::password('password', ['placeholder' => 'Password', 'class' => 'form-field', 'required' => 'required']) }}
             </div>
             <div>
-                {{ Form::submit('Sign Up') }}
+                {{ Form::submit('Sign Up', ['class' => 'btn red xxl']) }}
             </div>
             <div>
                 @if($errors->any())
-                <h4>{{$errors}}</h4>
+                <p class='xs'>{{$errors}}</p>
                 @endif
             </div>
-            <p>Creating an account means you are ok with our Terms of Service and Privacy Policy.
-            </p>
-            <p>Already have an account? <a href="login">Sign in</a></p>
+            <p class='xs'>Creating an account means you are o'k with our <a class="link">Terms of Service</a> and <a class="link">Privacy Policy</a>.</p>
+            <br/>
+            <p class='xs'>Already have an account? <a href="login" class="link">Sign in</a></p>
         {{ Form::close() }}
 @stop
