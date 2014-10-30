d9bd5e8d776ad846cbbfe9a692aca92398edfa2e
Ksenia Karelskaya <kusksu@gmail.com>
2014-10-30 20:17:00 +0100

    Styled header, article and prepared template for questions

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -2,7 +2,17 @@
 
 @section('content')
     <img src="assets/images/header.gif" class="cover image"/>
-    
+    <div class="section group" id="welcome">
+        <div class="col span_3_of_3">
+            <h1 class="h-white">Cookie Jar is for Creators and Makers</h1>
+            <p class="p-white">Do you need a model for your photoshoot? </p>
+            <p class="p-white">Or you are a web ninja in need of project for your portfolio?</p>
+            <p class="p-white">Here you can ask for help your fellow students!</p>
+            <br/>
+            <br/>
+            <a href="signup" class="btn xxxl red">Sign Up</a>
+        </div>
+    </div>
     <div class="section">
         <div class="col span_1_of_3">
             <h2>Latest Questions</h2>
@@ -12,33 +22,27 @@
         </div> 
     </div>
         <div class="section group">
-            <div class="col span_1_of_3">
-            This is column 1
-            </div>
-            <div class="col span_1_of_3">
-            This is column 2
-            </div>
-            <div class="col span_1_of_3">
-            This is column 3
-            </div>
+
+            @include('pages.partials.question')
+
+            @include('pages.partials.question')
+
+            @include('pages.partials.question')
         </div>
         <div class="section group">
-            <div class="col span_1_of_3">
-            This is column 1
-            </div>
-            <div class="col span_1_of_3">
-            This is column 2
-            </div>
-            <div class="col span_1_of_3">
-            This is column 3
-            </div>
+            @include('pages.partials.question')
+            @include('pages.partials.question')
+            @include('pages.partials.question')
         </div>
         <div class="section">
             <div class="col span_3_of_3">
                 <h2>Story: Natan van Boven</h2>
                 <p>Student of engineering Natan helped Alison with her art project and this is how their friendship was born!</p>
                 <br/>
-                <img src="assets/images/article.gif" class="image"/>
+                <img src="assets/images/article.gif" class="image article"/>
+                <div class="read">
+                    <a href="" class="btn white">Read The Story</a>
+                </div>
             </div>
         </div>
 
---
+++ app/views/pages/partials/question.blade.php
@@ -0,0 +1,7 @@
+<div class="col span_1_of_3">
+	<img src="assets/images/q1.gif" class="question">
+	<h4><a href="">Title needed!</a></h4>
+	<small>Submited 2h ago</small>
+	<small>by <a href="">Name Name</a> </small>
+	<p>Help needed till <span>12.12.2014</span></p>
+</div>
