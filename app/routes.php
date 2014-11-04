7e40a415eb44f2cfdaffead084d5db9b2e2a3b14
William Blommaert <williamblommaert@gmail.com>
2014-11-04 21:28:31 +0100

    Cleaned up routes, added PagesController for static pages

--- app/routes.php
+++ app/routes.php
@@ -3,11 +3,26 @@
 // Resources
 Route::resource('sessions', 'SessionController');
 Route::resource('user', 'UserController');
-Route::resource('project', 'ProjectController');
-Route::resource('conversation', 'ConversationController');
-Route::resource('message', 'MessageController');
 
-// Logging in/out using Cookie Jar account
+// Search routes
+Route::post('search', 'SearchController@search');
+Route::get('search/results', 'SearchController@results');
+
+// User routes
+Route::get('profile', 'UserController@profile');
+Route::get('profile/{id}', 'UserController@profilebyid');
+Route::get('signup', 'UserController@create');
+Route::post('signup', 'UserController@store');
+
+// Question routes
+Route::get('questions', 'QuestionController@index');
+Route::get('questions/new', 'QuestionController@create')->before('auth');
+
+// Message routes
+Route::get('messages', 'MessageController@home');
+Route::post('messages/reply', 'MessageController@reply')->before('auth');
+
+// Authentication routes
 Route::get('/', function()
 {
     $data = array();
@@ -17,37 +32,14 @@ Route::get('/', function()
     }
     return View::make('pages.home', array('data'=>$data));
 });
-Route::get('login', 'SessionController@create');
+
 Route::get('logout', function() {
     Auth::logout();
     return Redirect::to('/')->with('message', 'You have been logged out.');
 });
 
-// Search routes
-Route::post('search', 'SearchController@search');
-Route::get('search/results', 'SearchController@results');
+Route::get('login', 'SessionController@create');
 
-// User routes
-Route::get('profile', 'UserController@profile');
-Route::get('profile/{id}', 'UserController@profilebyid');
-Route::get('settings', 'UserController@settings')->before('auth');
-Route::get('signup', 'UserController@create');
-Route::post('signup', 'UserController@store');
-
-// Project routes
-//Route::get('projects', 'ProjectController@home');
-Route::get('projects/{id}', 'ProjectController@details');
-Route::get('projects/{id}/edit', 'ProjectController@edit')->before('auth');
-Route::get('questions', 'ProjectController@index');
-Route::get('questions/new', 'ProjectController@create')->before('auth');
-Route::get('story/{id}', 'ProjectController@story');
-Route::get('story/new', 'ProjectController@story')->before('auth');
-
-// Message routes
-Route::get('messages', 'MessageController@home');
-Route::post('messages/reply', 'MessageController@reply')->before('auth');
-
-// Facebook routes
 Route::get('login/fb', function() {
     $facebook = new Facebook(Config::get('facebook'));
     $params = array(
@@ -56,22 +48,6 @@ Route::get('login/fb', function() {
     );
     return Redirect::away($facebook->getLoginUrl($params));
 });
-
-/*
-// Display FB user info as object
-Route::get('login/fb/callback', function() {
-    $code = Input::get('code');
-    if (strlen($code) == 0) return Redirect::to('/')->with('message', 'There was an error communicating with Facebook');
-
-    $facebook = new Facebook(Config::get('facebook'));
-    $uid = $facebook->getUser();
-
-    if ($uid == 0) return Redirect::to('/')->with('message', 'There was an error');
-
-    $me = $facebook->api('/me');
-
-    dd($me);
-});*/
 
 Route::get('login/fb/callback', function() {
     $code = Input::get('code');
