f6bf567448bb58f32713fff651cca243d7369462
William Blommaert <williamblommaert@gmail.com>
2014-11-04 01:21:28 +0100

    Created logic for unfinished project retrieval, added eager relations in models

--- app/controllers/ProjectController.php
+++ app/controllers/ProjectController.php
@@ -2,84 +2,39 @@
 
 class ProjectController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /project
-	 *
-	 * @return Response
-	 */
 	public function index()
 	{
+        $projects = Project::where('finished', '=','0')->with('user','projectcategories');
 		$data = Auth::user();
-		return View::make('questions.overview', array('data' => $data));
+		return View::make('questions.overview', compact('data', 'projects'));
 	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /project/create
-	 *
-	 * @return Response
-	 */
 	public function create()
 	{
 		$data = Auth::user();
 		return View::make('questions.new', array('data' => $data));
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /project
-	 *
-	 * @return Response
-	 */
 	public function store()
 	{
 		//
 	}
 
-	/**
-	 * Display the specified resource.
-	 * GET /project/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function show($id)
 	{
 		//
 	}
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /project/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function edit($id)
 	{
 		//
 	}
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /project/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function update($id)
 	{
 		//
 	}
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /project/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
 	{
 		//
--- app/models/Project.php
+++ app/models/Project.php
@@ -2,4 +2,14 @@
 
 class Project extends \Eloquent {
 	protected $fillable = [];
+
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
+
+    public function projectcategories()
+    {
+        return $this->hasMany('ProjectCategoriesLink');
+    }
 }
