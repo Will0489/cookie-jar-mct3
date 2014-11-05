0370f99906cff56ace4b72de6fd16281aeaa554c
Ksenia Karelskaya <kusksu@gmail.com>
2014-11-05 15:08:51 +0100

    Responsive menu: show/hide working, styling should be fixed

--- public/dev/js/main.js
+++ public/dev/js/main.js
@@ -3,7 +3,7 @@ var changeClass = function (r, className1, className2) {
 	var regex = new RegExp("(?:^|\\s+)" + className1 + "(?:\\s+|$)");
 
 	if( regex.test(r.className) ) {
-		r.className = r.className.replace(regex,' '+className2+' ');
+		r.className = r.className.replace(regex,''+className2+'');
 	}
 	else{
 		r.className = r.className.replace(new RegExp("(?:^|\\s+)" + className2 + "(?:\\s+|$)"),' '+className1+' ');
@@ -16,25 +16,23 @@ var menuElements = document.getElementById('menu');
 menuElements.insertAdjacentHTML('afterBegin',
 	'<li><a href="#" alt="menu" id="menutoggle" class="navtoggle" aria-hidden="true"><span class="icon"><i aria-hidden="true" class="icon-menu"></i></span></a></li>');
 
-var changeVisibility = function () {
-	var menuItems = menuElements.getElementsByTagName('li');
-	var num = menuItems.length;
-	var menuIcons = menuItems.getElementsByTagName('a');
-	for (var i = 0; i < num; i++) {
-		//menuIcons[i] = menuItems.getElementsByTagName('a');
-		if ( menuIcons[i].style.display  === 'none' ) {
-			menuIcons[i].style.display = 'inline-block';
-		}
-		else {
-			menuIcons[i].style.display  = 'none';
-		}
-	}
-};
 
 //  Toggle the class on click to show / hide the menu
 document.getElementById('menutoggle').onclick = function() {
-	changeClass(this, 'navtoggle active', 'navtoggle');
-	changeVisibility();
+	
+	var menuIcons = menuElements.getElementsByTagName('a');
+	var num = menuIcons.length;
+	for (var i = 0; i < num; i++) {
+		if (menuIcons[i].className == 'navtoggle' || menuIcons[i].className == ' navtoggle active ') {
+			changeClass(this, 'navtoggle active', 'navtoggle');
+		}else{
+			if (menuIcons[i].className == 'show') {
+				menuIcons[i].setAttribute("class", "hide");
+			}else{
+				menuIcons[i].setAttribute("class", "show");
+			}
+		}
+	}
 };
 
 // http://tympanus.net/codrops/2013/05/08/responsive-retina-ready-menu/comment-page-2/#comment-438918
