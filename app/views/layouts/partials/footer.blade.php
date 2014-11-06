8380ccc20303380caf2a07dcbb22e6121ae21f5d
Ksenia Karelskaya <kusksu@gmail.com>
2014-11-06 12:12:14 +0100

    Styled responsive footer

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -1,4 +1,15 @@
 <footer>
+	<div class="section group">
+		<div class="col span_1_of_3">
+			<a class="centered white-txt xs" href="questions">Help</a>
+		</div>
+		<div class="col span_1_of_3">
+			<a class="centered white-txt xs"  href="questions">About</a>
+		</div>
+		<div class="col span_1_of_3">
+			<a class="centered white-txt xs"  href="questions">Terms</a>
+		</div>
+	</div>
 	         
 </footer>
 {{ HTML::script('assets/js/main.min.js') }}
--- app/views/layouts/partials/head.blade.php
+++ app/views/layouts/partials/head.blade.php
@@ -2,4 +2,3 @@
 	<title>@yield('meta-title', 'Cookie Jar')</title>
 	{{ HTML::style('assets/css/normalize.css') }}
 	{{ HTML::style('assets/css/style.css') }}
-
