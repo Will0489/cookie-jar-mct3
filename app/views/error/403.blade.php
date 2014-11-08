4be6ee0c85e5e204d30101d292df775ca8cc286c
Ksenia Karelskaya <kusksu@gmail.com>
2014-11-08 21:31:26 +0100

    Added error pages 404 and 403

---
+++ app/views/error/403.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+
+@section('content')
+
+<div class="section group">
+	<div class="section-content">
+
+			<div class="msg-body">
+				<h2>Woops! 403 ...</h2>
+				<p>You are truly a rebel! But you shouldn't try to get this file! Try something <a href="/">here</a>?</p>
+			</div>
+
+	</div>
+</div>
+@stop
---
+++ app/views/error/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+
+@section('content')
+
+<div class="section group">
+	<div class="section-content">
+
+			<div class="msg-body">
+				<h2>Woops! 404 ...</h2>
+				<p> You've asked us for something we don't have:( Try something <a href="/">here</a>?</p>
+			</div>
+
+	</div>
+</div>
+@stop
