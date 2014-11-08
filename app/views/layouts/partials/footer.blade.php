ef8a9f3ecb89c32edfb27ed4622bfc5b28d20c76
William Blommaert <williamblommaert@gmail.com>
2014-11-08 20:35:37 +0100

    Added missing user variable in views, typo in footer routing

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -1,13 +1,13 @@
 <footer>
 	<div class="section group">
 		<div class="col span_1_of_3">
-			<a class="centered white-txt xs" href="help">Help</a>
+			<a class="centered white-txt xs" href="/help">Help</a>
 		</div>
 		<div class="col span_1_of_3">
-			<a class="centered white-txt xs"  href="about">About</a>
+			<a class="centered white-txt xs"  href="/about">About</a>
 		</div>
 		<div class="col span_1_of_3">
-			<a class="centered white-txt xs"  href="tos">Terms</a>
+			<a class="centered white-txt xs"  href="/tos">Terms</a>
 		</div>
 	</div>
 	         
