5be9604edf45c699cce219605c52352d3bd1e7bc
Ksenia Karelskaya <kusksu@gmail.com>
2014-11-10 15:45:34 +0100

    Prepared row template for msging pg

--- app/controllers/MessageController.php
+++ app/controllers/MessageController.php
@@ -4,7 +4,15 @@ class MessageController extends \BaseController {
 
 	public function index()
 	{
-		//TODO: Message overview logic
+		if(Auth::check())
+        {
+            $data = Auth::user();
+            $questions = Question::where("user_id", "=", Auth::id())->get();
+
+            return View::make('messages.overview', compact('data', 'questions'));
+        } else {
+            return View::make('error.guest');
+        }
 	}
 
 	public function store()
--- app/views/messages/overview.blade.php
+++ app/views/messages/overview.blade.php
@@ -1,1 +1,25 @@
-app/views/messages/overview.blade.php
+@extends('layouts.default')
+
+@section('content')
+	<div class="section group">
+		<div class="col span_1_of_5"></div>
+		<div class="col span_3_of_5">
+			<h2 class="qHeading s-tag">Messages</h2>
+			<div class="msgBox">
+				<!-- column for open dialogs with users -->
+				<div>
+					
+				</div>
+				<!-- column for messaging -->
+				<div>
+
+					<!-- row for msg input field -->
+					<div>
+						
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+@stop
