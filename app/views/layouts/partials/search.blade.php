32bea697b47cfe705ccdc31477ef55dffc6c6521
Ksenia Karelskaya <kusksu@gmail.com>
2014-11-11 11:12:36 +0100

    changed search form

--- app/views/layouts/partials/search.blade.php
+++ app/views/layouts/partials/search.blade.php
@@ -1,7 +1,12 @@
 <div id="sb-search" class="sb-search">
-	<form action="" autocomplete="on">
+<!-- 	<form action="" autocomplete="on">
 			<input class="sb-search-input" placeholder="Find you what?" type="text" value="" name="search" id="search">
 			<input class="sb-search-submit" type="submit" value="">
 			<span class="icon"><i aria-hidden="true" class="icon-search"></i></span>
-	</form>
+	</form> -->
+	{{ Form::open() }}
+		{{Form::input('search', 'q', null, ['placeholder' => "Find you what?", 'class'=>"sb-search-input"])}}
+		<span class="icon"><i aria-hidden="true" class="icon-search"></i></span>
+	{{ Form::close() }}
+
 </div>
