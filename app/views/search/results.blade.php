bb4a453d857bc58e90d310479c8db8c067b051cf
Ksenia Karelskaya <kusksu@gmail.com>
2014-11-12 12:22:31 +0100

    Bug fixes

--- app/views/search/results.blade.php
+++ app/views/search/results.blade.php
@@ -6,11 +6,11 @@
 @endif
 
 <div class="section group">
-	@if ($questions->count()) {
-		<div class="col span_5_of_5 msg-block">
+	@if ($questions->count())
+		<div class="col span_5_of_5 ">
 			<h2 class="qHeading push">We could find these for ya!</h2>
 		</div>
-		<div class="col span_5_of_5 msg-block">
+		<div class="col span_5_of_5">
 			@foreach ($questions as $question)
 				@include('questions.partials.question')
 			@endforeach
@@ -19,6 +19,7 @@
 		<div class="col span_5_of_5 msg-block">
 			<p>We couldn't find anything like that :(</p>
 		</div>		
-	</div>
+	
 	@endif
+</div>
 @stop
